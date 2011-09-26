ee56cd06fe3d39c5ebe057b397d9162e661df403
Luper Rouch <luper.rouch@gmail.com>
2011-09-26 14:57:15 +0200

    properly handling kill in the Bonjour server

--- server/leechyremote/discover.py
+++ server/leechyremote/discover.py
@@ -7,21 +7,21 @@ class BonjourServer(Greenlet):
 
     def __init__(self, name, regtype, port):
         super(BonjourServer, self).__init__()
-        self.stopped = False
         self.service_fd = pybonjour.DNSServiceRegister(name=name, regtype=regtype,
                 port=port, callBack=self.register_callback)
 
     def _run(self):
-        while not self.stopped:
-            ready = select.select([self.service_fd], [], [])
-            if self.service_fd in ready[0]:
-                pybonjour.DNSServiceProcessResult(self.service_fd)
-        self.service_fd.close()
+        try:
+            while True:
+                ready = select.select([self.service_fd], [], [])
+                if self.service_fd in ready[0]:
+                    pybonjour.DNSServiceProcessResult(self.service_fd)
+        except:
+            raise
+        finally:
+            self.service_fd.close()
 
-    def stop(self):
-        self.stopped = True
-
-    def register_callback(self, service, flags, error_code, name, regtype, domain):
+    def register_callback(self, service_fd, flags, error_code, name, regtype, domain):
         if error_code != pybonjour.kDNSServiceErr_NoError:
             raise ServiceRegistrationError("can't register service '%s', "
                     "error code: %s" & (regtype, error_code))
